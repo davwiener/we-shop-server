f0cd67e6a18acaef5f3815c20df69ccba03f3dd9
snirco <cohensnir@gmail.com>
2020-06-17 21:42:58 +0300

    created auctions

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuctionsModule } from './auctions/auctions.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuctionsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
