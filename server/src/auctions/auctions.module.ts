74dc1e1f92fc7bcbba5215f550f39b85de109756
snirco <>
2020-07-18 00:04:35 +0300

    finished auth added auth guards for auctions controller updated port for server

--- server/src/auctions/auctions.module.ts
+++ server/src/auctions/auctions.module.ts
@@ -3,9 +3,10 @@ import { AuctionsController } from './auctions.controller';
 import { AuctionsService } from './auctions.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Auction } from './auction.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Auction])],
+  imports: [TypeOrmModule.forFeature([Auction]), AuthModule],
   controllers: [AuctionsController],
   providers: [AuctionsService]
 })
