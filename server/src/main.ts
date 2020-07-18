d839b869d780b53a63edf0fd8734c1acb1efd6ae
snirco <>
2020-07-18 15:22:32 +0300

    added basic logging

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap')
   const app = await NestFactory.create(AppModule);
   await app.listen(5000);
+  logger.log('app is renning on port 5000')
 }
 bootstrap();
