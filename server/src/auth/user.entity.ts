c877dfb02fe1ad1dcc84062b011d1ae37ce8d7e3
snirco <>
2020-07-27 19:28:37 +0300

    test deploy

--- server/src/auth/user.entity.ts
+++ server/src/auth/user.entity.ts
@@ -1,5 +1,5 @@
 import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, OneToMany  } from 'typeorm'
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcryptjs'
 import { Auction } from 'src/auctions/auction.entity';
 import { Product } from 'src/products/product.entity';
 
--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,12 @@ async function bootstrap() {
   const logger = new Logger('bootstrap')
   const app = await NestFactory.create(AppModule);
   const serverConfig = config.get('server')
+
+  // if (process.env.NODE_ENV === 'development') {
+    app.enableCors()
+  // } else {
+    // app.enableCors({ origin: serverConfig.origin })
+  // }
   await app.listen(serverConfig.port);
   logger.log(`app is renning on port ${serverConfig.port}`)
 }
